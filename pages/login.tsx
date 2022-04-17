aeb3391d25877b7b808386e6d8573ba33dc9bd37
Philipp Kastl <picastle@posteo.de>
2022-04-18 00:59:38 +0200

    add custom auth functions

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,10 +14,14 @@ function Login() {
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
     if (login) {
+      await signIn(email, password)
+    } else {
+      await signUp()
     }
   }
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
